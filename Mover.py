b377623de724af0e2dd13efbedc981eb936abc9b
Iván R <irt@MacBook-Pro-de-Ivan.local>
2026-04-06 16:46:23 -0600

    config render

--- Mover.py
+++ Mover.py
@@ -201,5 +201,7 @@ def descargar(id_sesion):
 
 
 # ----------- RUN -----------
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050, debug=True)
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host="0.0.0.0", port=port)
